410a8b921056d93e16eed6bab66a0241fb7701db
Ayush Soni <ayushsonid078@gmail.com>
2025-02-04 18:24:58 +0000

    data types

---
+++ javascript/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Ayush"
+let age = 21
+let isLoggedin = false
+
+// number => 2 to power 53
+// bigint
+// string => "" / ''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique     // these are primitive datatypes
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
