37ab0f05da7ef3726a67d25c5f9f2edd7a2c74c5
Ayush Soni <ayushsonid078@gmail.com>
2025-02-24 18:21:20 +0000

    Conversion of datatypes

---
+++ javascript/conversion.js
@@ -0,0 +1,29 @@
+let score = "99a";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) // if score is "99abs"  return NaN (not a number)
+
+// "33" => number
+// "99abc" => NaN
+// true => 1; false => 0;
+// null => 0;
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true And 0 => false
+// "" => false
+// "ayush" => true
+
+let someNum = 45;
+
+let stringNumber = String(someNum);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
